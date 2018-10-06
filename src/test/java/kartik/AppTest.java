de4ebf669299ac64c576d580093c06294f44360d
sharmakartik <kartik.gs@outlook.com>
2018-10-06 13:05:41 +0530

    selenium

--- src/test/java/kartik/AppTest.java
+++ src/test/java/kartik/AppTest.java
@@ -3,6 +3,7 @@ package kartik;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
@@ -21,5 +22,21 @@ public class AppTest
     public void kk()
     {
         System.out.println("junit test");
+    }
+
+    @Test
+    public void testSelenium() {
+
+        String chromeDriverPath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+        ChromeDriver cDriver = new ChromeDriver();
+
+
+        cDriver.get("http://www.google.com/");
+        cDriver.close();
+        cDriver.quit();
+
     }
 }
