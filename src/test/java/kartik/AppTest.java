87900224cb69182c52ce1fc876fbada877a3ab29
sharmakartik <kartik.gs@outlook.com>
2018-11-19 15:46:07 +0530

    jar file push without selenium

--- src/test/java/kartik/AppTest.java
+++ src/test/java/kartik/AppTest.java
@@ -3,7 +3,7 @@ package kartik;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 /**
  * Unit test for simple App.
@@ -24,19 +24,4 @@ public class AppTest
         System.out.println("junit test");
     }
 
-    @Test
-    public void testSelenium() {
-
-        String chromeDriverPath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
-
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-
-        ChromeDriver cDriver = new ChromeDriver();
-
-
-        cDriver.get("http://www.google.com/");
-        cDriver.close();
-        cDriver.quit();
-
-    }
 }
